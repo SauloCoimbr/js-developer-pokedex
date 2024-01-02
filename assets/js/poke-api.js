881971808d7a7ffcbd7033ce1562fe588ccdee1f
Saulo Coimbra <saulo.coimbra1@gmail.com>
2024-01-02 16:24:39 -0300

    Added the Skills

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -5,6 +5,9 @@ function convertPokeApiDetailToPokemon(pokeDetail) {
     const pokemon = new Pokemon()
     pokemon.number = pokeDetail.id
     pokemon.name = pokeDetail.name
+   
+
+    console.log(pokeDetail)
 
     const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
     const [type] = types
@@ -12,8 +15,14 @@ function convertPokeApiDetailToPokemon(pokeDetail) {
     pokemon.types = types
     pokemon.type = type
 
-    pokemon.photo = pokeDetail.sprites.other.dream_world.front_default
+    const abilities = pokeDetail.abilities.map((abilitySlot) => abilitySlot.ability.name)
+    const [ability] = abilities
 
+    pokemon.abilities = abilities
+    pokemon.ability = ability
+
+    pokemon.photo = pokeDetail.sprites.other.dream_world.front_default
+    
     return pokemon
 }
 
